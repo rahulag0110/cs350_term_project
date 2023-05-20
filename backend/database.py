34f294ca3b03c147c36831c995ea18fc4bfc0021
Samadov Fuad <samedovfuad2002@gmail.com>
2023-05-20 23:51:09 +0900

    added models and created simple register api function

--- backend/database.py
+++ backend/database.py
@@ -1,39 +1,49 @@
-from model import Todo
+from models import Todo
 import motor.motor_asyncio
 
 
-# client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://simple:simple@cluster0.duulzju.mongodb.net/")
-
-database = client.TodoList
-collection = database.todo
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
+# client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://simple:simple@cluster0.duulzju.mongodb.net/")
 
 
-async def fetch_one_todo(title):
-    document = await collection.find_one({"title":title})
+database = client.ProjectDatabase
+collection_users = database.Users
+collection_events = database.Events
+
+
+
+# async def fetch_one_todo(title):
+#     document = await collection.find_one({"title":title})
+#     return document
+
+
+# async def fetch_all_todos():
+#     todos = []
+#     cursor = collection.find({})
+#     async for document in cursor:
+#         todos.append(Todo(**document))
+#     return todos
+
+
+
+async def register_user(user):
+    document = user
+    result = await collection_users.insert_one(document)
     return document
 
 
-async def fetch_all_todos():
-    todos = []
-    cursor = collection.find({})
-    async for document in cursor:
-        todos.append(Todo(**document))
-    return todos
+# async def create_todo(todo):
+#     document = todo
+#     result = await collection.insert_one(document)
+#     return document
 
 
-async def create_todo(todo):
-    document = todo
-    result = await collection.insert_one(document)
-    return document
+# async def update_todo(title, desc):
+#     await collection.update_one({"title": title}, {"$set":{"description": desc}})
+#     document = await collection.find_one({"title": title})
+#     return document
 
 
-async def update_todo(title, desc):
-    await collection.update_one({"title": title}, {"$set":{"description": desc}})
-    document = await collection.find_one({"title": title})
-    return document
-
-
-async def remove_todo(title):
-    await collection.delete_one({"title":title})
-    return True
+# async def remove_todo(title):
+#     await collection.delete_one({"title":title})
+#     return True
--- backend/main.py
+++ backend/main.py
@@ -4,11 +4,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
 from database import *
 
-from model import Todo
+from models import Todo, User
 
 
-# client = AsyncIOMotorClient('mongodb://localhost:21017')
-client = AsyncIOMotorClient("mongodb+srv://simple:simple@cluster0.duulzju.mongodb.net/")
+client = AsyncIOMotorClient('mongodb://localhost:21017')
+# client = AsyncIOMotorClient("mongodb+srv://simple:simple@cluster0.duulzju.mongodb.net/")
 
 origins = ["https://localhost:300"]
 
@@ -28,48 +28,58 @@ def read_root():
     return {"Ping":"Pong"}
 
 
-@app.get("/api/todo")
-async def get_todo():
-    response = await fetch_all_todos()
-    return response
+# @app.get("/api/todo")
+# async def get_todo():
+#     response = await fetch_all_todos()
+#     return response
 
 
-@app.get("/api/todo{title}")
-async def get_todo_by_id(title):
-    response = await fetch_one_todo(title)
-    if response:
-        return response
-    else:
-        raise HTTPException(404, "no todo with this title")
+# @app.get("/api/todo{title}")
+# async def get_todo_by_id(title):
+#     response = await fetch_one_todo(title)
+#     if response:
+#         return response
+#     else:
+#         raise HTTPException(404, "no todo with this title")
 
 
-@app.get("/api/todo/{title}", response_model=Todo)
-async def get_todo_by_title(title):
-    response = await fetch_one_todo(title)
-    if response:
-        return response
-    raise HTTPException(404, f"There is no todo with the title {title}")
+# @app.get("/api/todo/{title}", response_model=Todo)
+# async def get_todo_by_title(title):
+#     response = await fetch_one_todo(title)
+#     if response:
+#         return response
+#     raise HTTPException(404, f"There is no todo with the title {title}")
 
 
-@app.post("/api/todo/", response_model=Todo)
-async def post_todo(todo: Todo):
-    response = await create_todo(todo.dict())
+# @app.post("/api/todo/", response_model=Todo)
+# async def post_todo(todo: Todo):
+#     response = await create_todo(todo.dict())
+#     if response:
+#         return response
+#     raise HTTPException(400, "Something went wrong")
+
+
+
+@app.post("/user/register", response_model=User)
+async def user_register(user: User):
+    response = await register_user(user.dict())
     if response:
         return response
     raise HTTPException(400, "Something went wrong")
 
 
-@app.put("/api/todo/{title}/", response_model=Todo)
-async def put_todo(title: str, desc: str):
-    response = await update_todo(title, desc)
-    if response:
-        return response
-    raise HTTPException(404, f"There is no todo with the title {title}")
+
+# @app.put("/api/todo/{title}/", response_model=Todo)
+# async def put_todo(title: str, desc: str):
+#     response = await update_todo(title, desc)
+#     if response:
+#         return response
+#     raise HTTPException(404, f"There is no todo with the title {title}")
 
 
-@app.delete("/api/todo/{title}")
-async def delete_todo(title):
-    response = await remove_todo(title)
-    if response:
-        return "Successfully deleted todo"
-    raise HTTPException(404, f"There is no todo with the title {title}")
+# @app.delete("/api/todo/{title}")
+# async def delete_todo(title):
+#     response = await remove_todo(title)
+#     if response:
+#         return "Successfully deleted todo"
+#     raise HTTPException(404, f"There is no todo with the title {title}")
