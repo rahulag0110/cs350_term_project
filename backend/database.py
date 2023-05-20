85c0facbe85ba90014778c3d2e5192e18513dba7
Samadov Fuad <samedovfuad2002@gmail.com>
2023-05-20 17:22:49 +0900

    working example

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,8 @@
-from cs350_term_project.backend.model import Todo
+from model import Todo
 import motor.motor_asyncio
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb"//localhost:21017')
 
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
 database = client.TodoList
 collection = database.todo
 
@@ -11,6 +11,7 @@ async def fetch_one_todo(title):
     document = await collection.find_one({"title":title})
     return document
 
+
 async def fetch_all_todos():
     todos = []
     cursor = collection.find({})
@@ -18,13 +19,19 @@ async def fetch_all_todos():
         todos.append(Todo(**document))
     return todos
 
+
 async def create_todo(todo):
     document = todo
     result = await collection.insert_one(document)
     return document
+
 
 async def update_todo(title, desc):
     await collection.update_one({"title": title}, {"$set":{"description": desc}})
     document = await collection.find_one({"title": title})
     return document
 
+
+async def remove_todo(title):
+    await collection.delete_one({"title":title})
+    return True
