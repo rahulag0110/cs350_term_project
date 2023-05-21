fd9a50b7273612175c519cb0f79734151a4f0a81
Samadov Fuad <samedovfuad2002@gmail.com>
2023-05-21 20:37:03 +0900

    added skeleton code for user login and event creation

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,7 @@
 import motor.motor_asyncio
 from bson.objectid import ObjectId
+from models.user_models import User
+from models.event_models import Event
 
 
 client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
@@ -14,27 +16,42 @@ def UserHelper(User) -> dict:
         "password": str(User["password"])
     }
 
+def EventHelper(Event) -> dict:
+    return{
+        "_id": str(Event["_id"]),
+        "host_id": str(Event["host_id"]),
+        "name": str(Event["name"]),
+        "open_date": str(Event["open_date"]),
+        "close_date": str(Event["close_date"]), 
+        "reward_box_id": str(Event["reward_box_id"])
+    }
 
 database = client.ProjectDatabase
 collection_users = database.Users
 collection_events = database.Events
 
 
-async def register_user(user):
-    document = user
-    result = await collection_users.insert_one(document)
-    return document
+async def register_user(user: User):
+    result = await collection_users.insert_one(user)
+    if result:
+        registered_user = await collection_users.find_one(user)
+        return UserHelper(registered_user)['_id']
 
 
-async def login_user(email):
-    result = await collection_users.find_one({"email": email})
+async def login_user(email, password):
+    result = await collection_users.find_one({"email": email, "password": password})
     if result:
         return UserHelper(result)["_id"]
-
     
-
 
 async def deregister_user(user_id: ObjectId):
     document = user_id
     result = await collection_users.delete_one({"_id": ObjectId(user_id)})
     return document
+
+
+async def create_event(event: Event):
+    result = await collection_events.insert_one(event)
+    if result:
+        registered_event = await collection_events.find_one(event)
+        return EventHelper(registered_event)['_id']
