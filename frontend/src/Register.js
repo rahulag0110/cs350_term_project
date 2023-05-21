081b42b3514a5c8ffa022988558edb3726873ff6
Rahul Agrawal <rahul@Rahuls-MacBook-Pro.local>
2023-05-22 02:48:26 +0900

    Register Code (Success)
    1. Added a Register.js file in frontend/src which contains the logic for registration.
    2. Added the Register component to App.js to be rendered in the frontend

---
+++ frontend/src/Register.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { useRef, useState, useEffect } from "react";
+
+const Register = () => {
+
+    const [name, setName] = useState([{}])
+    const [email, setEmail] = useState([{}])
+    const [password, setPassword] = useState([{}])
+
+    // useEffect(() => {
+    //     axios.get('http://')
+    //     .then(response)
+    // })
+
+    const registerHandler = () => {
+        axios.post('http://127.0.0.1:8000/user/register', {'name':name, 'email': email, 'password': password})
+        .then(res => console.log(res))
+    }
+
+
+    return (
+        <>
+        <input
+            onChange= {
+            e => setName(e.target.value)
+
+            }
+            placeholder='Name'
+        />
+
+        <input
+            onChange= {
+            e => setEmail(e.target.value)
+
+            }
+            placeholder='Email'
+        />
+
+        <input
+            onChange= {
+            e => setPassword(e.target.value)
+            }
+            placeholder='Password'
+        />
+    
+        <button onClick={registerHandler}>Register</button>
+        </>
+    
+    
+    )
+}
+
+export default Register
