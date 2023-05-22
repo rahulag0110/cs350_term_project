ff7b63a27fc9c648a65aff630a47d0debc6a1e00
Samadov Fuad <samedovfuad2002@gmail.com>
2023-05-22 15:05:34 +0900

    added validation for login

--- backend/database.py
+++ backend/database.py
@@ -5,17 +5,8 @@ import motor.motor_asyncio
 client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://simple:simple@cluster0.duulzju.mongodb.net/")
 
 
-def RewardBoxHelper(RewardBox) -> dict:
-    return{
-        "_id": str(RewardBox["_id"]),
-        "event_id": str(RewardBox["event_id"]),
-        "reward_id": str(RewardBox["reward_id"]),
-        "reward": str(RewardBox["reward"]),
-        "claim_info": str(RewardBox["claim_info"])
-    }
-
-
 database = client.ProjectDatabase
 collection_users = database.Users
 collection_events = database.Events
 collection_prizes = database.Prizes
+collection_winners = database.Winners
