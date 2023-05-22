d1c48304bf47e2b334f33400c4b173497f4e7480
Samadov Fuad <samedovfuad2002@gmail.com>
2023-05-22 17:43:07 +0900

    solved the issue

--- backend/controllers/c_application.py
+++ backend/controllers/c_application.py
@@ -18,7 +18,12 @@ async def apply_event(application: Application):
         applied_event = await collection_applications.find_one(application)
         return ApplicationHelper(applied_event)['_id']
     
-async def delete_application(application_id: str):
-    document = application_id
-    result = await collection_applications.delete({"_id": ObjectId(application_id)})
-    return document
+
+
+async def deregister_application(application_id: ObjectId):
+    result = await collection_applications.delete_one({"_id": ObjectId(application_id)})
+    if result:
+        response_data = {"status": "SUCCESS"}
+    else:
+        response_data = {"status": "FAIL"}
+    return response_data
