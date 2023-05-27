0e18e753672ec0509abd8e015c39158fed61c811
Samadov Fuad <samedovfuad2002@gmail.com>
2023-05-28 01:57:30 +0900

    added approve application function

--- backend/routers/r_application.py
+++ backend/routers/r_application.py
@@ -7,17 +7,23 @@ router = APIRouter(prefix="/application", tags=['Application'])
 
 
 @router.post("/apply", summary="Apply for an event")
-async def event_apply(application: Application):
+async def application_apply_event(application: Application):
     response = await apply_event(application.dict())
     return response
 
 
 @router.delete("/delete_application", summary="Delete an application")
 async def application_deregister(application_id):
-    response = await deregister_application(application_id)
+    response = await deregister(application_id)
     return response
 
 @router.get("/event_applications", summary="Fetch all applications for an event")
 async def application_get_event_applications(event_id):
-    response = await event_applications(event_id)
+    response = await get_event_applications(event_id)
+    return response
+
+
+@router.put("/approve", summary="Approve an application")
+async def application_approve(application_id):
+    response = await approve(application_id)
     return response
