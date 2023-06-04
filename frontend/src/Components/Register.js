812dd414d9efcfdae967cfeaa5b588122b80e180
Rahul Agrawal <rahul@Rahuls-MacBook-Pro.local>
2023-06-04 23:12:07 +0900

    Some context changes

--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -7,10 +7,6 @@ const Register = () => {
     const [email, setEmail] = useState([{}])
     const [password, setPassword] = useState([{}])
 
-    // useEffect(() => {
-    //     axios.get('http://')
-    //     .then(response)
-    // })
 
     const registerHandler = () => {
         axios.post('http://127.0.0.1:8000/user/register', {'name':name, 'email': email, 'password': password})
