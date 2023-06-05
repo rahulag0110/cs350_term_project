28cf07c9363540b6a61d3bc28fa8f296b55e24ec
Samadov Fuad <samedovfuad2002@gmail.com>
2023-06-05 16:56:44 +0900

    separated styles into css files

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,48 +1,20 @@
 import { Link } from "react-router-dom";
 import HomeAfterLogin from "./HomeAfterLogin";
 import HomeBeforeLogin from "./HomeBeforeLogin";
-
-const styles = {
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        backgroundColor: '#f2f2f2', // pastel grey
-    },
-    banner: {
-        backgroundColor: '#5B6C87', // pastel blue
-        color: 'white',
-        width: '100%',
-        padding: '10px 0',
-        position: 'absolute',
-        top: 0,
-        textAlign: 'center',
-        fontSize: '1.5em',
-    },
-    link: {
-        textDecoration: 'none',
-        margin: '10px 0',
-        padding: '10px 20px',
-        border: 'none',
-        borderRadius: '5px',
-        backgroundColor: '#FFABAB', // pastel red
-        color: 'white',
-        cursor: 'pointer',
-    },
-};
+import { UserContext } from "../Hooks/UserContext";
+import { useContext } from "react";
+import "../Styles/Home.css"
 
 const Home = () => {
     const {user, setUser} = useContext(UserContext);
 
     return (
-        <>
-        <Link to="/login">Login</Link>
+        <div className="container">
+        <Link to="/login" className="link">Login</Link>
         <p></p>
-        <Link to="/events">Events</Link>
+        <Link to="/events" className="link">Events</Link>
 
-        </>
+        </div>
     )
 }
 
