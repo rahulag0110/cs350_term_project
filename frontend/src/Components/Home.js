6aba4fab198f2bd790009e1f951436497980f035
Rahul Agrawal <rahulag0110@gmail.com>
2023-06-16 18:49:26 +0900

    able to fetch events

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -2,17 +2,21 @@ import { Link } from "react-router-dom";
 import HomeAfterLogin from "./HomeAfterLogin";
 import HomeBeforeLogin from "./HomeBeforeLogin";
 import { UserContext } from "../Hooks/UserContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "../Styles/Home.css"
 
 const Home = () => {
     const {user, setUser} = useContext(UserContext);
+    console.log(user)
 
     return (
         <div className="container">
-        <Link to="/login" className="link">Login</Link>
-        <p></p>
-        <Link to="/events" className="link">Events</Link>
+            <h1>This is Home page</h1>
+            <h1>user_in_storage = {window.localStorage.getItem("current_user")}</h1>
+            <h1>user_in_context = {user}</h1>
+            <Link to="/login" className="link">Login</Link>
+            <p></p>
+            <Link to="/register" className="link">Register</Link>
 
         </div>
     )
--- frontend/src/Components/HomeAfterLogin.js
+++ frontend/src/Components/HomeAfterLogin.js
@@ -1,20 +1,38 @@
 // import axios from "axios";
 // import { useRef, useState, useEffect } from "react";
+import axios, { all } from "axios";
 import { Link } from "react-router-dom";
 import { UserContext } from "../Hooks/UserContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "../Styles/HomeAfterLogin.css"
+import ShowEvents from "./ShowEvents";
 
 
 const HomeAfterLogin = () => {
 
-    const [user, setUser] = useContext(UserContext);
+    const {user, setUser} = useContext(UserContext);
+
+    const [events, setEvents] = useState([])
+
+    const getEventsHandler = () => {
+        axios.post('http://127.0.0.1:8000/user/participating_events', {'user_id': '646b321936e5d064bb48de1a'})
+        .then(res => {
+            console.log(res);
+            console.log(res.data.events);
+            const allevents = res.data['events'];
+            setEvents(allevents)
+        })
+    }
+
 
     return (
         <div className="container">
             <h1 className="title">Welcome User!!!</h1>
         <h2>{user}</h2>
-            <Link className="link" to="/events">Events</Link>
+            
+        <button onClick={() => <ShowEvents />}>Current Events</button>
+        {/* <ShowEvents /> */}
+
         </div>
     )
 }
