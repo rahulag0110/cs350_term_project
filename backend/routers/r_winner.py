f59531a1dcf495f057e94bbdc0758d300e6ee636
Samadov Fuad <samedovfuad2002@gmail.com>
2023-06-20 16:35:14 +0900

    winner thing

--- backend/routers/r_winner.py
+++ backend/routers/r_winner.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 from controllers.c_winner import *
+from models.event_models import EventId
 
 
 router = APIRouter(prefix="/winner", tags=['Winner'])
 
 @router.post("/select", summary = "Event winners")
-async def event_winners(event_id: str):
-    response = await select_winners(event_id)
+async def event_winners(event_id: EventId):
+    response = await select_winners(event_id.dict())
     return response
