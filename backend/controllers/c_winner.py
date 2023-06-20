5d9a1ad5e26da285b2db771f34fd67d57f69af80
Samadov Fuad <samedovfuad2002@gmail.com>
2023-06-20 16:35:14 +0900

    winner thing

--- backend/controllers/c_winner.py
+++ backend/controllers/c_winner.py
@@ -2,18 +2,21 @@ from database import collection_applications, collection_prizes, collection_winn
 import random
 from helpers.winner_helpers import *
 from helpers.prize_helpers import *
+from models.event_models import EventId
+from bson.objectid import ObjectId
+from helpers.event_helpers import EventIdHelper
 
 
-async def select_winners(event_id: str):
+async def select_winners(event_id: EventId):
     async for winner in collection_winners.find():
         if event_id == WinnerHelper(winner)["event_id"]:
             response_data = {"status": "FAIL", "msg": "Winners already selected"}
             return response_data
     applications = []
-    async for application in collection_applications.find({"event_id": event_id, "status": True}, {"participant_id": 1, "event_id": 1}):
+    async for application in collection_applications.find({"event_id": ObjectId(EventIdHelper(event_id)["event_id"]), "status": True}, {"participant_id": 1, "event_id": 1}):
         applications.append(TempWinnerHelper(application))
     prizes = []
-    async for prize in collection_prizes.find({"event_id": event_id}):
+    async for prize in collection_prizes.find({"event_id": ObjectId(EventIdHelper(event_id)["event_id"])}):
         prizes.append(PrizeHelper(prize))
     winners = random.sample(applications, len(prizes))
     ctr = 0
